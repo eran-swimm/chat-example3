3f051717f6a87742c0f60731879ff83a586487ff
Eran Keydar <ekeydar@gmail.com>
2020-02-18 22:16:25 +0200

    p3

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class IndexView(TemplateView):
+    template_name = 'chat/index.html'
+
+
+class RoomView(TemplateView):
+    template_name = 'chat/room.html'
+
